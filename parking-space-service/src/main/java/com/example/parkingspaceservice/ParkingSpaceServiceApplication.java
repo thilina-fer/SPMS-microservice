5de3bf40d3a14dae73c3a5385bad313123b0a5db
thilina-fer <dilshanthilina1010@gmail.com>
2026-08-10 09:20:08 +0530

    add parking-space-service module with initial setup and configuration

---
+++ parking-space-service/src/main/java/com/example/parkingspaceservice/ParkingSpaceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkingspaceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSpaceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingSpaceServiceApplication.class, args);
+    }
+
+}
