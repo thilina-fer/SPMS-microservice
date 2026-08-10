6870bcae12ebe2ef67fa4558b75198dcc3974a68
thilina-fer <dilshanthilina1010@gmail.com>
2026-08-10 11:43:59 +0530

    feat : add DTO classes for user login, registration, and response handling

---
+++ user-service/src/main/java/lk/ijse/spms/userservice/dto/ResponseDTO.java
@@ -0,0 +1,3 @@
+package lk.ijse.spms.userservice.dto;
+
+public record ResponseDTO(int code, String message, Object data) {}
---
+++ user-service/src/main/java/lk/ijse/spms/userservice/dto/UserRegisterDTO.java
@@ -0,0 +1,30 @@
+package lk.ijse.spms.userservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lk.ijse.spms.userservice.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegisterDTO {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    private Role role;
+}
