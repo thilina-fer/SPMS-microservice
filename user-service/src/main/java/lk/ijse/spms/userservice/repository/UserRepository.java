e0ecab58fee447a251722e1ac7aacb9e7a484c79
thilina-fer <dilshanthilina1010@gmail.com>
2026-08-10 11:46:17 +0530

    feat : add UserRepository interface for user data access with email lookup methods

---
+++ user-service/src/main/java/lk/ijse/spms/userservice/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package lk.ijse.spms.userservice.repository;
+
+import lk.ijse.spms.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
