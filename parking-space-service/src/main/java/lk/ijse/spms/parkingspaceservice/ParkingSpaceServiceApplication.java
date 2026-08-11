ea555955c8db3f2851435fe1992765a7c3491cfc
thilina-fer <dilshanthilina1010@gmail.com>
2026-08-11 09:20:54 +0530

    feat : add main application class and test for Parking Space Service

---
+++ parking-space-service/src/main/java/lk/ijse/spms/parkingspaceservice/ParkingSpaceServiceApplication.java
@@ -0,0 +1,12 @@
+package lk.ijse.spms.parkingspaceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSpaceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingSpaceServiceApplication.class, args);
+    }
+}
---
+++ parking-space-service/src/test/java/lk/ijse/spms/parkingspaceservice/ParkingSpaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.spms.parkingspaceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSpaceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
