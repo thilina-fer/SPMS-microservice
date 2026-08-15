d62f066880b30818c02f0607cf11575c829c0be1
thilina-fer <dilshanthilina1010@gmail.com>
2026-08-15 09:17:40 +0530

    feat : add PaymentServiceApplication and its test class for application startup and context loading

---
+++ payment-service/src/test/java/lk/ijse/spms/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.spms.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
